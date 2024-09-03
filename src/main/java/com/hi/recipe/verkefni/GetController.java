d527010b901a6cc17f91c1117443d6bf6a425f92
sandraliv <sls40@hi.is>
2024-09-03 15:30:10 +0000

    GET update
    Made two endpoints, check /Get and / endpoints

--- src/main/java/com/hi/recipe/verkefni/GetController.java
+++ src/main/java/com/hi/recipe/verkefni/GetController.java
@@ -1,12 +1,19 @@
 package com.hi.recipe.verkefni;
 
+import com.hi.recipe.verkefni.klasar.Recipe;
+import com.hi.recipe.verkefni.klasar.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetController {
     @RequestMapping("/")
-    public String getGreeting(){
-        return "Hi, its me Sandra";
+    public User getGreeting(){
+        return new User("Sandra Liv", "Rice Pudding");
+    }
+
+    @RequestMapping("/Get")
+    public Recipe getBody(){
+       return new Recipe("Eggjahræra", "2 egg");
     }
 }
---
+++ src/main/java/com/hi/recipe/verkefni/klasar/Recipe.java
@@ -0,0 +1,21 @@
+package com.hi.recipe.verkefni.klasar;
+
+public class Recipe {
+    private String name;
+    private String description;
+
+    public Recipe(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
+
