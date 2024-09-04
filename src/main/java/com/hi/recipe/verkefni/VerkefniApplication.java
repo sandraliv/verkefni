9a38e9e18c8174f31cf435801c73944f5df8c9e4
Ásdís Halldóra L Stefánsdóttir <103222479+asdis96@users.noreply.github.com>
2024-09-04 15:45:00 +0000

    strengur
    commit hjá Ásdísi

--- src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
+++ src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
@@ -9,6 +9,7 @@ public class VerkefniApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VerkefniApplication.class, args);
 		System.out.println("Sturla skrifaði streng");
+		System.out.println("Ásdís skrifaði streng nr 2");
 	}
 
 }
