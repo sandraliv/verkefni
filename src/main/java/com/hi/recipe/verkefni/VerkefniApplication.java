c268c5b114473be5110de417abfadd5965e7ff33
sandraliv <sls40@hi.is>
2024-09-07 18:59:45 +0000

    added to gitignore
    maven versions different between us, hope this doesnot change anything for you, trying to gitignore it

--- src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
+++ src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VerkefniApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VerkefniApplication.class, args);
-		System.out.println("Sturla skrifaði streng");
-		System.out.println("Ásdís skrifaði streng nr 2");
-		System.out.println("Hilla mús er dúlla");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VerkefniApplication.class, args);
+        System.out.println("Sturla skrifaði streng");
+        System.out.println("Ásdís skrifaði streng nr 2");
+        System.out.println("Hilla mús er dúlla");
+    }
 }
