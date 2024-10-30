859fab6c366e484d2869df91250ef4a978416594
sturla-freyr <stullif@gmail.com>
2024-10-30 15:20:59 +0000

    bætti við gögnum úr DB á frontpage

--- src/main/java/com/hi/recipe/verkefni/controllers/ViewController.java
+++ src/main/java/com/hi/recipe/verkefni/controllers/ViewController.java
@@ -5,12 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
+import com.hi.recipe.verkefni.klasar.Recipe;
+import com.hi.recipe.verkefni.klasar.RecipeTag;
+import com.hi.recipe.verkefni.services.RecipeService;
+
+
 @Controller
 public class ViewController {
+    private final RecipeService recipeService;
 
+    public ViewController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("message", "Sturla er framendaforritari.");
+        model.addAttribute("recipes", recipeService.findAllPaginated());
         return "FrontPage";
     }
 }
