61ab1c18d11fa98c341230e5bd25407123233883
hildursvava <112590527+hildursvava@users.noreply.github.com>
2024-11-04 16:23:39 +0000

    gleymdi að importa map hehe

--- src/main/java/com/hi/recipe/verkefni/controllers/UserController.java
+++ src/main/java/com/hi/recipe/verkefni/controllers/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/users")
