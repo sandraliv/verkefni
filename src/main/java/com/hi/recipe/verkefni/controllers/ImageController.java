1a651398248370141aad3407a60204fafd569f6b
sandraliv <sls40@hi.is>
2024-11-05 11:25:56 +0000

    lagaði endpoint for upload

--- src/main/java/com/hi/recipe/verkefni/controllers/ImageController.java
+++ src/main/java/com/hi/recipe/verkefni/controllers/ImageController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/images")
+@RequestMapping("/recipes")
 public class ImageController {
 
     private final ImageService imageService;
