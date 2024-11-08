0f0613f267f116d748dfd78d6631d13428c2df4e
hildursvava <112590527+hildursvava@users.noreply.github.com>
2024-11-08 15:48:39 +0000

    login get mapping

--- src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
+++ src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
+
 @SpringBootApplication
 public class VerkefniApplication {
 
