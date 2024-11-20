0ab1504bcf198ec1423afc76c4463e3524331cdf
Ásdís Halldóra L Stefánsdóttir <103222479+asdis96@users.noreply.github.com>
2024-11-20 17:02:05 +0000

    laga

--- src/main/java/com/hi/recipe/verkefni/klasar/Category.java
+++ src/main/java/com/hi/recipe/verkefni/klasar/Category.java
@@ -8,9 +8,8 @@ public enum Category {
     DESSERT,
     BAKING;
 
-    // Method to return a human-readable version of the enum name
+
     public String getDisplayName() {
-        // Capitalize the first letter and replace underscores with spaces
         return name().charAt(0) + name().substring(1).toLowerCase().replace('_', ' ');
     }
 }
