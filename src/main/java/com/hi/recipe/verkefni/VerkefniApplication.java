115f4bde408e3ea589ff04805fec7e524798113e
sandraliv <sls40@hi.is>
2025-02-14 13:07:18 +0000

    tók ut env nauðsyn

--- src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
+++ src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
@@ -2,6 +2,7 @@ package com.hi.recipe.verkefni;
 
 import com.cloudinary.Cloudinary;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,16 +15,25 @@ public class VerkefniApplication {
         SpringApplication.run(VerkefniApplication.class, args);
     }
 
-    // Configure Cloudinary Bean with dotenv
+//    // Configure Cloudinary Bean with dotenv
+//    @Bean
+//    public Cloudinary cloudinary() {
+//        // Load environment variables from .env
+//        String currentDir = System.getProperty("user.dir");
+//        System.out.println("Current Working Directory: " + currentDir);
+//        Dotenv dotenv = Dotenv.load();
+//
+//
+//        // Initialize Cloudinary with CLOUDINARY_URL
+//        return new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+//    }
+
     @Bean
-    public Cloudinary cloudinary() {
-        // Load environment variables from .env
-        String currentDir = System.getProperty("user.dir");
-        System.out.println("Current Working Directory: " + currentDir);
-        Dotenv dotenv = Dotenv.load();
-
-
-        // Initialize Cloudinary with CLOUDINARY_URL
-        return new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+    public Cloudinary cloudinary(@Value("${CLOUDINARY_URL}") String cloudinaryUrl) {
+        if (cloudinaryUrl == null || cloudinaryUrl.isEmpty()) {
+            throw new IllegalStateException("CLOUDINARY_URL is not set");
+        }
+        return new Cloudinary(cloudinaryUrl);
     }
+
 }
