4d609b5e3b8df61989fd5865a626a2359b8afcdd
sandraliv <sls40@hi.is>
2025-02-14 15:30:55 +0000

    llal

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM maven:3-eclipse-temurin-17 AS build
 COPY . .
-RUN mvn clean package -Dskiptests
+RUN mvn clean package -DskipTests
 
 FROM eclipse-temurin:17-alpine
 COPY --from=build /target/*.jar verkefni.jar
--- src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
+++ src/main/java/com/hi/recipe/verkefni/VerkefniApplication.java
@@ -2,7 +2,6 @@ package com.hi.recipe.verkefni;
 
 import com.cloudinary.Cloudinary;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +14,16 @@ public class VerkefniApplication {
         SpringApplication.run(VerkefniApplication.class, args);
     }
 
-    @Value("${CLOUDINARY_URL}")
-    private String cloudinaryUrl;
-
+    // Configure Cloudinary Bean with dotenv
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(cloudinaryUrl);
+        // Load environment variables from .env
+        String currentDir = System.getProperty("user.dir");
+        System.out.println("Current Working Directory: " + currentDir);
+        Dotenv dotenv = Dotenv.load();
+
+
+        // Initialize Cloudinary with CLOUDINARY_URL
+        return new Cloudinary(dotenv.get("CLOUDINARY_URL"));
     }
 }
